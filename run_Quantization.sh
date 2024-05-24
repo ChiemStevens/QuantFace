51f0f24ee5d6ad1a66571b778ee23c4b5c8deba5
Chiem <c.p.stevens@student.tue.nl>
2024-05-24 14:36:25 +0200

    Change quantiza script

--- run_Quantization.sh
+++ run_Quantization.sh
@@ -1,4 +1,4 @@
 export OMP_NUM_THREADS=1
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 \
+CUDA_VISIBLE_DEVICES="" python -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 \
 --node_rank=0 --master_addr="127.0.0.1" --master_port=1255 train_quantization.py
 ps -ef | grep "train" | grep -v grep | awk '{print "kill -9 "$2}' | sh
